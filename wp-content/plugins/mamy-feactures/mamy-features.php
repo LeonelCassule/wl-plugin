5ff6c13ab51373e8f5fa17ed068cc68960e0c427
LeonelCassule <leonelcassule1994@gmail.com>
2024-08-07 23:52:49 +0100

    first commit

---
+++ wp-content/plugins/mamy-feactures/mamy-features.php
@@ -0,0 +1,17 @@
+<?php
+/**
+* Plugin Name:  Mamy-Features
+* Plugin URI:   https://example.com/plugins/the-basic/
+* Description:  Handle the basics with this plugin.
+* Version:      1.10.3
+* Requires at least: 5.2
+* Reaquires PHP: 8.2
+* Author:      wl-plugin
+* Author URL:  https://linkedin.com/leonelCassul
+* License:     GPL v2 or later
+* License URL: https://www.gnu.org/licenses/gpl-2.0.html
+* Update URI:  https://examples.com/my-plugin/
+* Text Domian: my-basics-plugin
+* Domain Path: /languages
+
+*/
